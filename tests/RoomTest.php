bf6c87018b2c850994789350992875d577c2d475
Enzo Huynh <enzo_huynh@hotmail.com>
2019-06-28 16:51:04 +0200

    TestCase

--- tests/RoomTest.php
+++ tests/RoomTest.php
@@ -1,10 +1,7 @@
 <?php
 
 use App\Room;
-<<<<<<< HEAD
 use Illuminate\Foundation\Testing\TestCase;
-=======
->>>>>>> d59299894e25b31580d124e3a4732b7908e52026
 
 abstract class RoomTest extends Laravel\Lumen\Testing\TestCase
 {
