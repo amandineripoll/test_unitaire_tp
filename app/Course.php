74fbbdabaa4428083350b80adb75c63c5fe80c0b
Amandine <amandine-ripoll@outlook.fr>
2019-06-28 15:21:49 +0200

    course model

---
+++ app/Course.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model 
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'date', 'room_id', 'teacher_id'
+    ];
+
+     /**
+     * A course belongs to a room
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+    public function room()
+    {
+        return $this->belongsTo('App\Room');
+    }
+
+         /**
+     * A course belongs to a teacher
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+    public function teacher()
+    {
+        return $this->belongsTo('App\Teacher');
+    }
+}
